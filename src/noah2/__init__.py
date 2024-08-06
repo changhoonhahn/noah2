3a9ddc6dc4eaf02218b1e15f6b165824612561dc
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2024-08-06 17:20:55 +0200

    fixing init

---
+++ src/noah2/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__author__ = "ChangHoon Hahn"
+__email__ = "changhoon.hahn@princeton.edu"
+__uri__ = "https://github.com/changhoonhahn/noah2"
+__license__ = "MIT"
+__description__ = "Python Package for NOAH2"
+__copyright__ = "Copyright 2024 ChangHoon Hahn"
