041238152dd9992276febeb43840a7d085c785ff
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2024-08-22 17:45:10 +0200

    deploying on hpcs

--- bin/nde_support/hpc.py
+++ bin/nde_support/hpc.py
@@ -7,11 +7,11 @@ python script to deploy jobs on della-gpu
 import os, sys 
 
 
-def train_NDE_optuna(iseed, hr=12, gpu=True): 
+def train_NDE_nonpart(iseed, hr=12, gpu=True): 
     ''' train NDE training
     '''
-    jname = "NDE.%i" % iseed, 
-    ofile = "o/_NDE.%i" % iseed, 
+    jname = "NDE_nonpart.%i" % iseed
+    ofile = "o/_NDE_nonpart.%i" % iseed
 
     script = '\n'.join([
         "#!/bin/bash", 
@@ -30,7 +30,7 @@ def train_NDE_optuna(iseed, hr=12, gpu=True):
         "source ~/.bashrc", 
         "conda activate sbi", 
         "",
-        "python nde.py", 
+        "python nde_nonpart.py", 
         "",
         'now=$(date +"%T")', 
         'echo "end time ... $now"', 
@@ -45,4 +45,44 @@ def train_NDE_optuna(iseed, hr=12, gpu=True):
     return None
 
 
-train_NDE_optuna(0, hr=1, gpu=True)
+def train_NDE_binary(iseed, binary, hr=12, gpu=True): 
+    ''' train NDE training
+    '''
+    jname = "NDE_binary%i.%i" % (binary, iseed)
+    ofile = "o/_NDE_binary%i.%i" % (binary, iseed)
+
+    script = '\n'.join([
+        "#!/bin/bash", 
+        "#SBATCH -J %s" % jname,
+        "#SBATCH --nodes=1", 
+        "#SBATCH --time=%s:59:59" % str(hr-1).zfill(2),
+        "#SBATCH --export=ALL", 
+        ['', "#SBATCH --gres=gpu:1"][gpu], 
+        "#SBATCH --output=%s" % ofile, 
+        "#SBATCH --mail-type=all",
+        "#SBATCH --mail-user=chhahn@princeton.edu",
+        "", 
+        'now=$(date +"%T")', 
+        'echo "start time ... $now"', 
+        "", 
+        "source ~/.bashrc", 
+        "conda activate sbi", 
+        "",
+        "python nde_binary.py %i" % binary, 
+        "",
+        'now=$(date +"%T")', 
+        'echo "end time ... $now"', 
+        ""]) 
+
+    # create the script.sh file, execute it and remove it
+    f = open('script.slurm','w')
+    f.write(script)
+    f.close()
+    os.system('sbatch script.slurm')
+    os.system('rm script.slurm')
+    return None
+
+
+train_NDE_nonpart(0, hr=1, gpu=True)
+for code in [ 0,  2,  8,  9, 10, 27, 64, 66, 72, 75]: 
+    train_NDE_binary(0, code, hr=1, gpu=True)
